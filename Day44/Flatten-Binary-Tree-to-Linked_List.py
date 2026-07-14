8991048c8fb079ce03cf52e5877c6e561806065c
Paramasivan02 <paramasivana02@gmail.com>
2026-07-14 21:49:12 +0530

    Day44 completed

---
+++ Day44/Flatten-Binary-Tree-to-Linked_List.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def flatten(self, root: 'TreeNode') -> None:
+        curr = root
+        
+        while curr:
+            if curr.left:
+                prev = curr.left
+                while prev.right:
+                    prev = prev.right
+                    
+                prev.right = curr.right
+                
+                curr.right = curr.left
+                
+                curr.left = None
+                
+            curr = curr.right
---
+++ Day44/Kth-Largest-in-a-Stream.py
@@ -0,0 +1,19 @@
+import heapq
+
+class KthLargest:
+    def __init__(self, k: int, nums: list[int]):
+        self.k = k
+        self.min_heap = []
+        
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        if len(self.min_heap) < self.k:
+            heapq.heappush(self.min_heap, val)
+            
+        elif val > self.min_heap[0]:
+            heapq.heappushpop(self.min_heap, val)
+            
+        return self.min_heap[0]
+
